7e37fcafbc42556803d29ae958bdf0b99b6bb38b
Piyush Bachani <Piyush>
2024-03-04 16:52:40 +0530

    Done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import LaptopsList from './components/laptopsList';
 import AddLaptop from './components/addLaptop';
 import UpdateLaptop from './components/updateLaptop';
 import Login from './components/login';
+// import Cookies from 'js-cookie';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <img src="https://cdn-icons-png.flaticon.com/128/689/689355.png" alt="Laptop Icon" id="laptopIcon" />
           <h1>Gaming Laptop Search Station</h1>
           <Link to="/login"><button id='login'>Login</button></Link>
+          {/* <button id='logout' onClick={()=>Cookies.remove("Username")}>Logout</button> */}
         </header>
         <Routes>
           <Route path='/' element={<LaptopsList />} />
--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -1,51 +1,57 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import './login.css';
-import Cookies from 'js-cookie';
+  import React, { useState } from 'react';
+  import axios from 'axios';
+  import './login.css';
+  import Cookies from 'js-cookie';
 
-function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [buttonStatus,setButtonStatus] = useState(false)
 
-  const viewPassword = () => {
-    const x = document.getElementById('x');
-    if (x.type === 'password') {
-      x.type = 'text';
-    } else {
-      x.type = 'password';
-    }
-  };
+    const viewPassword = () => {
+      const x = document.getElementById('x');
+      if (x.type === 'password') {
+        x.type = 'text';
+      } else {
+        x.type = 'password';
+      }
+    };
 
-  const loginFunc = async (e) => {
-    e.preventDefault();
-      const response = await axios.post("http://localhost:3000/logins", { Email: email, Password: password });
-      console.log(response.data);
-      Cookies.set('Email',email)
-      alert("You have logged in..")
+    const loginFunc = async (e) => {
+      e.preventDefault();
+      setButtonStatus(true) 
+        const response = await axios.post("http://localhost:3000/logins", { Email: username, Password: password });
+        console.log(response.data);
+        Cookies.set('Username',username)
+        localStorage.setItem("token",response.data.token)
+        Cookies.set("token",response.data.token)
+        alert("You have logged in..")
+        window.location.href="/"
+    };
+
+    const logoutFunc = () => {
+      setButtonStatus(true)
+      Cookies.remove("Username")
+      Cookies.remove("token")
+      alert("You have logged out..")
       window.location.href="/"
-  };
+    };
 
-  const logoutFunc = () => {
-    Cookies.remove("Email");
-    alert("You have logged out..");
-    window.location.href="/"
-  };
+    return (
+      <div id="loginPage">
+        <h1>Log In/Sign Up</h1>
+        <form id="loginArea">
+          <label>User Name</label><br /><input type="text" required onChange={(e) => setUsername(e.target.value)} /><br /><br />
+          <label>Password</label><br /><input type="password" id="x" required onChange={(e) => setPassword(e.target.value)} /><br /><br />
+          <input type="checkbox" onClick={viewPassword} id='checkBox' /><label id='p'>Show Password</label>
+          <br /><br />
+          <div id='buttons'>
+            <button onClick={loginFunc} disabled={buttonStatus}>Login</button> <button onClick={logoutFunc} disabled={buttonStatus}>Logout</button>
+          </div>
+          <br /><br />
+        </form>
+      </div>
+    );
+  }
 
-  return (
-    <div id="loginPage">
-      <h1>Log In/Sign Up</h1>
-      <form id="loginArea">
-        <label>Email</label><br /><input type="text" required onChange={(e) => setEmail(e.target.value)} /><br /><br />
-        <label>Password</label><br /><input type="password" id="x" required onChange={(e) => setPassword(e.target.value)} /><br /><br />
-        <input type="checkbox" onClick={viewPassword} id='checkBox' /><label id='p'>Show Password</label>
-        <br /><br />
-        <div id='buttons'>
-          <button onClick={loginFunc}>Login</button> <button onClick={logoutFunc}>Logout</button>
-        </div>
-        <br /><br />
-      </form>
-    </div>
-  );
-}
-
-export default Login;
+  export default Login;
